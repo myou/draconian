38da52c50b7d1caa6080ef9589d6c9c42578d75b
Max You <max.you15@gmail.com>
2013-11-16 20:22:53 -0800

    enable optional parameters

--- test/normal_params.js
+++ test/normal_params.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 
 var Drac = require('../');
 
-describe('regular functions, typeof rules', function() {
+describe('typeof rules', function() {
   var testFunc = Drac.define({
     params: {
       num1: { type: 'number' },
@@ -34,5 +34,27 @@ describe('regular functions, typeof rules', function() {
     expect(function() {
       testFunc(1, 2, 3);
     }).to.throw(Error);
+  });
+});
+
+describe('typeof rules with optional parameters', function() {
+  var testFunc = Drac.define({
+    params: {
+      num1: {
+        type: 'number'
+      },
+      num2: {
+        type: 'number'
+      },
+      str: {
+        type: 'string',
+        optional: true
+      }
+    }
+  }, function(num1, num2, str) { return num1 + num2 + str; });
+
+  it('should not throw error on missing optional parameters', function () {
+    var result = testFunc(1, 2);
+    expect(result).to.be.NaN;
   });
 });
