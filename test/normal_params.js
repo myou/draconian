35d6a3cb68304c7a514bf275c183382eb557a3fe
Max You <max.you15@gmail.com>
2013-11-18 22:43:50 -0800

    enable instance parameter checking

--- test/normal_params.js
+++ test/normal_params.js
@@ -57,3 +57,35 @@ describe('type rules with optional parameters', function() {
     expect(result).to.be.NaN;
   });
 });
+
+describe('instance rules', function() {
+  var testFunc = Drac.define({
+    params: {
+      arr: { instance: Array },
+      index: { type: 'number' }
+    }
+  }, function(arr, index) { return arr[index]; });
+
+  it('should give correct result', function() {
+    var result = testFunc([1, 2], 1);
+    expect(result).to.equal(2);
+  });
+
+  it('should throw error on missing parameters', function () {
+    expect(function() {
+      testFunc([1, 2]);
+    }).to.throw(Error);
+  });
+
+  it('should throw error on extra parameters', function () {
+    expect(function() {
+      testFunc([1, 2], 1, 3);
+    }).to.throw(Error);
+  });
+
+  it('should throw error on invalid parameters', function () {
+    expect(function() {
+      testFunc(1, 2);
+    }).to.throw(Error);
+  });
+});
