df673cdae974ebb8ac6e68a22869d7fe6605a7fc
Max You <max.you15@gmail.com>
2013-11-19 23:01:12 -0800

    enable parameter match

--- test/normal_params.js
+++ test/normal_params.js
@@ -89,3 +89,34 @@ describe('instance rules', function() {
     }).to.throw(Error);
   });
 });
+
+describe('match rules', function () {
+  var testFunc = Drac.define({
+    params: {
+      name: { match: /^abc[0-9]+/ }
+    }
+  }, function(name) { return 'hello ' + name; });
+
+  it('should give correct result', function() {
+    var result = testFunc('abc1230987');
+    expect(result).to.equal('hello abc1230987');
+  });
+
+  it('should throw error on missing parameters', function () {
+    expect(function() {
+      testFunc();
+    }).to.throw(Error);
+  });
+
+  it('should throw error on extra parameters', function () {
+    expect(function() {
+      testFunc('abc1340987', 3);
+    }).to.throw(Error);
+  });
+
+  it('should throw error on invalid parameters', function () {
+    expect(function() {
+      testFunc(3);
+    }).to.throw(Error);
+  });
+});
