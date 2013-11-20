314b43d6ce3976be58552cab3e227042f0e7e629
Max You <max.you15@gmail.com>
2013-11-19 23:17:14 -0800

    enable parameter not match

--- test/normal_params.js
+++ test/normal_params.js
@@ -102,21 +102,40 @@ describe('match rules', function () {
     expect(result).to.equal('hello abc1230987');
   });
 
-  it('should throw error on missing parameters', function () {
+  it('should throw error on non-string parameters', function () {
     expect(function() {
-      testFunc();
-    }).to.throw(Error);
-  });
-
-  it('should throw error on extra parameters', function () {
-    expect(function() {
-      testFunc('abc1340987', 3);
+      testFunc(3);
     }).to.throw(Error);
   });
 
   it('should throw error on invalid parameters', function () {
     expect(function() {
+      testFunc('bbc1230987');
+    }).to.throw(Error);
+  });
+});
+
+describe('not match rules', function () {
+  var testFunc = Drac.define({
+    params: {
+      name: { notMatch: /^abc[0-9]+/ }
+    }
+  }, function(name) { return 'hello ' + name; });
+
+  it('should give correct result', function() {
+    var result = testFunc('bbc1230987');
+    expect(result).to.equal('hello bbc1230987');
+  });
+
+  it('should throw error on non-string parameters', function () {
+    expect(function() {
       testFunc(3);
+    }).to.throw(Error);
+  });
+
+  it('should throw error on invalid parameters', function () {
+    expect(function() {
+      testFunc('abc1230987');
     }).to.throw(Error);
   });
 });
