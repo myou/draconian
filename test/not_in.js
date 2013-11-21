104ce303ec6658f3eaf1de86a0b73dc056a1581d
Max You <max.you15@gmail.com>
2013-11-21 05:08:42 -0800

    add parameter not in check

---
+++ test/not_in.js
@@ -0,0 +1,35 @@
+
+var expect = require('chai').expect;
+
+var Drac = require('../');
+
+describe('not in rules', function() {
+  var testFunc = Drac.define({
+    params: {
+      name: { notIn: ['hello', 3, false] },
+    }
+  }, function(name) { return 'name: ' + name; });
+
+  it('should give correct result', function() {
+    var result = testFunc(true);
+    expect(result).to.equal('name: true');
+  });
+
+  it('should throw error on missing parameters', function () {
+    expect(function() {
+      testFunc();
+    }).to.throw(Error);
+  });
+
+  it('should throw error on extra parameters', function () {
+    expect(function() {
+      testFunc(3, 'hello');
+    }).to.throw(Error);
+  });
+
+  it('should throw error on invalid parameters', function () {
+    expect(function() {
+      testFunc(3);
+    }).to.throw(Error);
+  });
+});
